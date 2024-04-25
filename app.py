4d5e3e2a6ab15d2e58d76e51048528a15ba1ade8
rodrigo.f.costa <rodrigo.f.costa@accenture.com>
2024-04-25 10:00:58 -0300

    Service with parameter passing in the body.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
@@ -8,9 +9,16 @@ def pessoa(id):
     soma = 1 + id
     return jsonify({'id':id, 'nome':'João', 'profissão':'Desenvolvedor'})
 
-@app.route('/soma/<int:valor1>/<int:valor2>/')
-def soma(valor1, valor2):
-    return jsonify({'soma':valor1 + valor2})
+# @app.route('/soma/<int:valor1>/<int:valor2>/')
+# def soma(valor1, valor2):
+#     return jsonify({'soma':valor1 + valor2})
+
+'''Método com passagem de parâmetros no body'''
+@app.route('/soma', methods=['POST'])
+def soma():
+    dados = json.loads(request.data)
+    total = sum(dados['valores'])
+    return jsonify({'soma':total})
 
 if __name__ == '__main__':
     app.run(debug=True)
